1208d7a74970978c230cf615b5909f9cf257b9fb
hailin6 <Sdu0512#>
2020-04-02 15:26:37 +0800

    valitation-xml

---
+++ validation-using-xml/src/main/java/com/example/validation/service/EditService.java
@@ -0,0 +1,12 @@
+package com.example.validation.service;
+
+import com.example.validation.model.Person;
+
+public interface EditService {
+	
+	
+	Person getPerson() ;
+
+	void savePerson(Person personBean);
+
+}
